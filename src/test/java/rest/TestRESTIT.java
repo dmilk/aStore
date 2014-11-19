fe60278d1cdf669f4bbf59dc36793409baed49c3
Oleg Sorokin <notreal@mail.ru>
2014-11-19 23:38:44 +0400

    add JerseyTest

--- src/test/java/rest/TestRESTIT.java
+++ src/test/java/rest/TestRESTIT.java
@@ -6,55 +6,89 @@
 package rest;
 
 //import com.sun.jersey.test.framework.JerseyTest;
+import entity.Route;
 import java.net.URI;
-import javax.json.JsonObject;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.junit.After;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.moxy.json.MoxyJsonFeature;
+import org.glassfish.jersey.server.ServerProperties;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.TestProperties;
+import org.glassfish.jersey.test.external.ExternalTestContainerFactory;
 import static org.junit.Assert.assertEquals;
-import org.junit.Before;
 import org.junit.Test;
+
 
 /**
  *
  * @author Notreal
  */
-public class TestRESTIT {
+public class TestRESTIT extends JerseyTest {
 
-    public static final URI BASE_URI = UriBuilder.fromUri("http://localhost").port(8081).build();
-    private HttpServer server;
-    private WebTarget target;
+    private final static Route ROUTE_1;
 
-    @Before
-    public void setUp() throws Exception {
-
-        ResourceConfig rc = new ResourceConfig(TestREST.class);
-        server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
-
-        server.start();
-        Client c = ClientBuilder.newClient();
-        target = c.target(BASE_URI);
+    static {
+        ROUTE_1 = new Route();
+        ROUTE_1.setId(1);
+        ROUTE_1.setName("One");
     }
 
-    @After
-    public void tearDown() throws Exception {
-        server.shutdown();
+    @Override
+    protected Application configure() {
+        enable(TestProperties.LOG_TRAFFIC);
+        enable(TestProperties.DUMP_ENTITY);
+
+        final MyApplication application = new MyApplication();
+        application.property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
+        return application;
     }
 
-    /**
-     * Test to see that the message "Got it!" is sent in the response.
-     */
-    @Test
-    public void testT() {
-        System.out.println("test");
-        String responseMsg = target.path("test/t").request().get(String.class);
-        System.out.println(responseMsg);
-        assertEquals("Hello", responseMsg);
+    @Override
+    protected void configureClient(final ClientConfig config) {
+        super.configureClient(config);
+
+        config.register(MoxyJsonFeature.class);
     }
     
+    @Override
+    protected URI getBaseUri() {
+        final UriBuilder baseUriBuilder = UriBuilder.fromUri(super.getBaseUri()).path("aStore");
+        final boolean externalFactoryInUse = getTestContainerFactory() instanceof ExternalTestContainerFactory;
+        return externalFactoryInUse ? baseUriBuilder.path("rest").build() : baseUriBuilder.build();
+    }
+    
+    @Test
+    public void testContactDoesNotExist() throws Exception {
+        final WebTarget target = target().
+                path("test");
+
+        // GET
+        System.out.println("target = " + target);
+        Response response = target.path("a").request(MediaType.APPLICATION_JSON_TYPE).get();
+        System.out.println(response);
+        Route route = response.readEntity(Route.class);
+        System.out.println(route);
+
+        // http://localhost:8080/aStore/rest/test/t
+        assertEquals(200, response.getStatus()); 
+
+//        Set<String> violationsMessageTemplates = getValidationMessageTemplates(response);
+//        assertEquals(1, violationsMessageTemplates.size());
+//        assertTrue(violationsMessageTemplates.contains("{contact.does.not.exist}"));
+//
+//        // DELETE
+//        response = target.path("1").request(MediaType.APPLICATION_JSON_TYPE).delete();
+//
+//        assertEquals(500, response.getStatus());
+//
+//        violationsMessageTemplates = getValidationMessageTemplates(response);
+//        assertEquals(1, violationsMessageTemplates.size());
+//        assertTrue(violationsMessageTemplates.contains("{contact.does.not.exist}"));
+    }
+
+
 }
