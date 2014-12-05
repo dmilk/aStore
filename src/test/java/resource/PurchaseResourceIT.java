2df11f763148b49c4596ff0880288e8c8d3b3dd6
Oleg Sorokin <notreal@mail.ru>
2014-12-05 12:36:26 +0400

    tmp-for-sync

--- src/test/java/resource/PurchaseResourceIT.java
+++ src/test/java/resource/PurchaseResourceIT.java
@@ -5,18 +5,16 @@
  */
 package resource;
 
-import entity.OrderedTicket;
+import entity.Route;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
 import javax.ws.rs.client.WebTarget;
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.Marshaller;
 import org.eclipse.persistence.jaxb.JAXBContextProperties;
+import org.eclipse.persistence.jaxb.MarshallerProperties;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
@@ -30,27 +28,38 @@ public class PurchaseResourceIT extends MyJerseyIT {
     public void testPlaceOrder() throws Exception {
         final WebTarget target = target().
                 path("purchase");
-        
+
 //        File file = new File("xxx");
 //        try (FileOutputStream output = new FileOutputStream(file)) {
 //            file.createNewFile();
 //            output.flush();
 //            output.close();
 //        }
-        
         try (FileInputStream is = new FileInputStream("profiles/integration-test/order_1.json")) {
-//            Map<String, Object> jaxbProperties = new HashMap<String, Object>();
-//            jaxbProperties.put(JAXBContextProperties.MEDIA_TYPE, MediaType.APPLICATION_JSON);
-            //jaxbProperties.put(JAXBContextProperties.MEDIA_TYPE, "application/json");
-//            jaxbProperties.put(JAXBContextProperties.JSON_INCLUDE_ROOT, false);
-//            JAXBContext jc = JAXBContext.newInstance(new Class[]{OrderedTicket.class}, jaxbProperties);
-//            Unmarshaller unmarshaller = jc.createUnmarshaller();
 
-            JsonReader jsonReader = Json.createReader(is);
+            Map<String, Object> jaxbProperties = new HashMap<>(2);
+            jaxbProperties.put(JAXBContextProperties.MEDIA_TYPE, "application/json");
 
-            JsonObject orderedTicket = jsonReader.readObject();
-            System.out.println(orderedTicket);
+            jaxbProperties.put(JAXBContextProperties.JSON_INCLUDE_ROOT, false);
 
+            JAXBContext jaxbContext = JAXBContext.newInstance(Route.class);
+            //Customer customer = (Customer) jaxbContext.createUnmarshaller().unmarshal(instanceDoc);
+
+            Route route = new Route(5);
+            route.setName("555");
+
+            Marshaller m = jaxbContext.createMarshaller();
+//            m.setProperty(MarshallerProperties.MEDIA_TYPE, "application/json");
+
+            m.marshal(route, System.out);
+
+//            System.out.println(orderedTicket.getTicketData());
+//            JsonReader jsonReader = Json.createReader(is);
+//
+//            JsonObject jsonObject = jsonReader.readObject();
+//            System.out.println(jsonObject);
+//            
+//            OrderedTicket orderedTicket = (OrderedTicket) unmarshaller.unmarshal(is);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         } catch (Exception e) {
@@ -67,5 +76,4 @@ public class PurchaseResourceIT extends MyJerseyIT {
 //                post(Entity.entity(user, MediaType.APPLICATION_JSON_TYPE));
 //        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
     }
-
 }
